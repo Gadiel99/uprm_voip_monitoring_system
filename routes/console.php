70474a42ba5854fed939ca7bcc49d3450c0eba26
Gadiel99 <gadiel.dejesus@upr.edu>
2025-10-19 16:53:26 -0400

    - Scheduled the etl command.

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schedule;
+
+Schedule::command('etl:run --since="5 minutes ago"')
+    ->everyFiveMinutes()
+    ->withoutOverlapping()
+    ->onFailure(function () {
+       Log::error('ETL command failed');
+    })
+    ->onSuccess(function () {
+       Log::info('ETL command completed successfully');
+    });
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
