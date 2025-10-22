b0644842c0eb5b6aa3138417cd3400fae7729fff
HectorSepul <hector.sepulveda5@upr.edu>
2025-10-21 22:10:14 -0400

    Created the routes for the frontend

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,29 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.home');
+});
+
+Route::get('/alerts', function () {
+    return view('pages.alerts');
+});
+
+Route::get('/devices', function () {
+    return view('pages.devices');
+});
+
+Route::get('/reports', function () {
+    return view('pages.reports');
+});
+
+Route::get('/admin', function () {
+    return view('pages.admin');
+});
+
+Route::get('/settings', function () {
+    return view('pages.settings');
+});
+
+Route::get('/help', function () {
+    return view('pages.help');
 });
