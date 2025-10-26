d612f661c88d18ca5af6f0cc49a90789b4417534
Gadiel99 <gadiel.dejesus@upr.edu>
2025-10-26 03:33:24 -0400

    - Refactor Devices model: organize properties and comment out unused extensions relationship

--- app/Models/Devices.php
+++ app/Models/Devices.php
@@ -6,10 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Devices extends Model
 {
+    protected $guarded = [];
+    protected $primaryKey = 'device_id';
+
+    protected $casts = [
+        'extension' => 'array', // Cast 'extension' JSON column to array
+    ];
     public function building(){
         return $this->belongsTo(Buildings::class, 'building_id');
     }
-    public function extensions(){
-        return $this->hasMany(Extensions::class, 'device_id');
-    }
+
+    // public function extensions(){
+    //     return $this->hasMany(Extensions::class, 'device_id');
+    // }
 }
