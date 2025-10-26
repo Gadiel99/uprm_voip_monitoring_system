3b63ecfe95ce0dd897e5cfcf7da4c088eda17cd3
Gadiel99 <gadiel.dejesus@upr.edu>
2025-10-26 03:34:31 -0400

    - Comment out unused properties and relationships in Extensions model and might be delete it in future.

--- app/Models/Extensions.php
+++ app/Models/Extensions.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Extensions extends Model
 {
-    protected $guarded = [];
-
-    public function device()
-    {
-        return $this->belongsToMany(Devices::class, 'device_id');
-    }
+    // protected $table = 'extension';
+    // protected $guarded = [];
+   
+    // public function device()
+    // {
+    //     return $this->belongsTo(Devices::class, 'device_id', 'device_id');
+    // }
 }
