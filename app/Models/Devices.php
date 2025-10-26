14435d38eefc540e206a3250fd35b072fb208f10
Gadiel99 <gadiel.dejesus@upr.edu>
2025-10-26 16:21:48 -0400

    - Fix casting of extensions property and update building relationship definition in Devices model

--- app/Models/Devices.php
+++ app/Models/Devices.php
@@ -10,13 +10,10 @@ class Devices extends Model
     protected $primaryKey = 'device_id';
 
     protected $casts = [
-        'extension' => 'array', // Cast 'extension' JSON column to array
+        'extensions' => 'array', // Cast 'extensions' JSON column to array
     ];
     public function building(){
-        return $this->belongsTo(Buildings::class, 'building_id');
+        return $this->belongsTo(Buildings::class, 'building_id', 'building_id');
     }
 
-    // public function extensions(){
-    //     return $this->hasMany(Extensions::class, 'device_id');
-    // }
 }
