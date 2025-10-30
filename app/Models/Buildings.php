966f0ae5a101227977cd809f082a39498fdcd664
Gadiel99 <gadiel.dejesus@upr.edu>
2025-10-29 22:23:22 -0400

    - Refactor Buildings model to enhance code clarity and improve documentation

--- app/Models/Buildings.php
+++ app/Models/Buildings.php
@@ -5,14 +5,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Networks;
 
+/**
+ * Definition of the Buildings model.
+ */
 class Buildings extends Model
 {
+    // Table associated with the model
+    protected $table = 'buildings';
+
+    // Mass assignable attributes
     protected $guarded = [];
+
+    // Primary key for the buildings table
     protected $primaryKey = 'building_id';
     
-
+    /**
+     * Function that defines the relationship to the Networks model.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany<Networks, Buildings, \Illuminate\Database\Eloquent\Relations\Pivot>
+     */
     public function networks()
     {
+        // Many-to-Many relationship with Networks.
         return $this->belongsToMany(Networks::class, 'building_networks', 'building_id', 'network_id');
     }
 
