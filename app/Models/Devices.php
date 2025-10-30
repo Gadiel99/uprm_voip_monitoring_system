f6285e141d558c451684c05a6ef83552e2620772
Gadiel99 <gadiel.dejesus@upr.edu>
2025-10-29 22:23:30 -0400

    - Refactor Devices model to improve documentation and enhance mass assignable attributes

--- app/Models/Devices.php
+++ app/Models/Devices.php
@@ -5,11 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Networks;
 
+/**
+ * Definition of the Devices model.
+ */
 class Devices extends Model
 {
+    // Table associated with the model
+    protected $table = 'devices';
+
+    // Primary key for the devices table
     protected $primaryKey = 'device_id';
-    protected $fillable = ['ip_address', 'network_id', 'status'];
+
+    // Mass assignable attributes
+    protected $fillable = ['ip_address', 'mac_address', 'network_id', 'status', 'is_critical'];
     
+    /**
+     * Function that defines the relationship to the Networks model.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo<Networks, Devices>
+     */
     public function network()
     {
 
@@ -17,9 +30,13 @@ class Devices extends Model
     
     }
 
+    /**
+     * This function defines the relationship to the Extensions model.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany<Extensions, Devices, \Illuminate\Database\Eloquent\Relations\Pivot>
+     */
     public function extensions()
     {
-
+        // Many-to-Many relationship with Extensions.
         return $this->belongsToMany(Extensions::class, 'device_extensions', 'device_id', 'extension_id')
         ->withTimestamps();
     }
