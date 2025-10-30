d53d2cbc09e8e93317fe019b396c5acda586873b
Gadiel99 <gadiel.dejesus@upr.edu>
2025-10-29 22:23:40 -0400

    - Refactor Extensions model to improve documentation and enhance code clarity

--- app/Models/Extensions.php
+++ app/Models/Extensions.php
@@ -6,12 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Extensions extends Model
 {
+    // Table associated with the model.
     protected $table = 'extensions';
+
+    // Primary key for the extensions table.
     protected $primaryKey = 'extension_id';
+
+    // Mass assignable attributes.
     protected $guarded = [];
+
+    /**
+     * This function defines the relationship to the Devices model.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany<Devices, Extensions, \Illuminate\Database\Eloquent\Relations\Pivot>
+     */
     public function devices()
     {
-
+        // Many-to-Many relationship with Devices.
         return $this->belongsToMany(Devices::class, 'device_extensions', 'extension_id', 'device_id')
         ->withTimestamps();
     }
