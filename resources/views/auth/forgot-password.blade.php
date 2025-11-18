c0ef6e2f229415868a9879ddd54f48e1d2d99656
Gadiel99 <gadiel.dejesus@upr.edu>
2025-11-18 12:18:53 -0400

    Add favicon link to the forgot password page

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forgot Password - UPRM VoIP Monitoring System</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/logo-uprm.png') }}">
 
     {{-- Bootstrap 5 --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
