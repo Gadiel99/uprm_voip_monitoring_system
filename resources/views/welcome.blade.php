a9599adfa00b1ba210a4736d8e0e10a716e57b7b
Gadiel99 <gadiel.dejesus@upr.edu>
2025-11-18 12:18:43 -0400

    Add favicon link to the welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,6 +71,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>UPRM VoIP Monitoring System</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/logo-uprm.png') }}">
 
     {{-- Bootstrap 5 CSS --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
